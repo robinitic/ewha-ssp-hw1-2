406ba8e4ad00f6321526a77d826297216b951c2b
robinitic <robinitic@gmail.com>
2015-03-25 13:20:06 +0900

    modify
    modify the project from lab 2_1 to lab 2_2(advance)

--- ewha-ssp-hw1-2.cpp
+++ ewha-ssp-hw1-2.cpp
@@ -1,13 +1,28 @@
 // Do not remove the include below
 #include "ewha-ssp-hw1-2.h"
-#define FRONT_LED 10
+#define DUTY_CYCLE 20
 #define REAR_LED 9
+int first=true;
+void PWM_Write(int pin,int percent){
+	int on_time=DUTY_CYCLE*percent/100.0;
+	int off_time=DUTY_CYCLE-on_time;
 
+	digitalWrite(pin,HIGH);
+	delay(on_time);
+	digitalWrite(pin,LOW);
+	delay(off_time);
+}
+void myAnalogWrite(int pin,int time){
+	for(int per=100;per>0;per--){
+		for(int t=0;t<(time/100);t+=DUTY_CYCLE){
+			PWM_Write(pin,per);
+		}
+	}
+}
 //The setup function is called once at startup of the sketch
 void setup()
 {
 // Add your initialization code here
-	pinMode(FRONT_LED,OUTPUT);
 	pinMode(REAR_LED,OUTPUT);
 }
 
@@ -15,23 +30,10 @@ void setup()
 void loop()
 {
 //Add your repeated code here
-	int curFront=0,curRear=255;
-	analogWrite(FRONT_LED,curFront);
-	analogWrite(REAR_LED,curRear);
-
-	for(int i=1;i<=255;i++){
-		curFront++;
-		curRear--;
-		analogWrite(FRONT_LED,curFront);
-		analogWrite(REAR_LED,curRear);
-		delay(5000/255);
-	}
-
-	for(int i=1;i<=255;i++){
-			curFront--;
-			curRear++;
-			analogWrite(FRONT_LED,curFront);
-			analogWrite(REAR_LED,curRear);
-			delay(5000/255);
-	}
+	if(first){
+			digitalWrite(REAR_LED,HIGH);
+			myAnalogWrite(REAR_LED,10000);
+			digitalWrite(REAR_LED,LOW);
+			first=false;
+		}
 }
